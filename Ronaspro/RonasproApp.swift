dc6d63cb44276dd6567cafafcdf8c4346b8e30ca
Sergei <volkovsa@vsa.su>
2021-01-24 22:33:09 +0500

    Initial Commit

---
+++ Ronaspro/RonasproApp.swift
@@ -0,0 +1,17 @@
+//
+//  RonasproApp.swift
+//  Ronaspro
+//
+//  Created by Sergey Volkov on 24.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct RonasproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
